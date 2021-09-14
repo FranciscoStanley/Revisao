f3dd92a3a1843341b227a860e29ff3d80f4495ab
Francisco Stanley Rodrigues Albuquerque <38919705+FranciscoStanley@users.noreply.github.com>
2021-09-14 01:08:05 -0300

    finalizando refatoração do case 3

--- Program.cs
+++ Program.cs
@@ -66,8 +66,33 @@ namespace Revisao
                         }
 
                         var mediaGeral = notaTotal / nrAlunos;
-                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral}");
+                        Conceito conceitoGeral;
 
+                        if (mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if(mediaGeral < 4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                          else if(mediaGeral < 6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                          else if(mediaGeral < 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                          else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
+
+                        Console.WriteLine("");
+                        
                         break;
 
                     default:
